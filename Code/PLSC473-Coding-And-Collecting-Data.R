fec1862f30f98d5b89340bec536e5130a58ea48e
Christopher Zorn <zorn@psu.edu>
2015-09-15 15:39:36 -0400

    Code

---
+++ Code/PLSC473-Coding-And-Collecting-Data.R
@@ -0,0 +1,23 @@
+#######################################
+# This is some code from PLSC 473,
+# dated September 15, 2015.
+#
+# Note that anything on a line
+# that starts with a "#" is a 
+# comment, and will be ignored by
+# the program (but not by you).
+#######################################
+#
+
+Baseball <- read.csv("Class Data/Baseball.csv")
+head(Baseball,10)
+
+Clerks <- read.csv("Class Data/AnnualClerks.csv")
+head(Clerks,10)
+
+TSCS <- read.csv("Class Data/CountryTSCS.csv")
+head(TSCS,10)
+tail(TSCS,12)
+
+Dyads <- read.csv("Class Data/Dyads1968.csv")
+head(Dyads,150)
